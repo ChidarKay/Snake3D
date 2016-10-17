738a7647380f756991609df241c4763c7f13eb8d
Tuomas Eero <eerotuo@gmail.com>
2016-10-17 14:23:12 +0300

    Initial commit

---
+++ Snake 3D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    float time;
+
+    public GameObject snakeBodiesInstance;
+
+    void Update() {
+        Controls();
+    }
+
+    void Controls() {
+        if (Input.GetKey("up")) {
+            snakeBodiesInstance.transform.position += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey("down")) {
+            snakeBodiesInstance.transform.position += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey("right")) {
+            snakeBodiesInstance.transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey("left")) {
+            snakeBodiesInstance.transform.position += new Vector3(-1, 0, 0);
+        }
+    }
+}
