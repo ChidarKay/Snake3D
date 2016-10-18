abff6117e3e0715b0b197002c5a349e463c13b50
Tuomas Eero <eerotuo@gmail.com>
2016-10-18 20:22:42 +0300

    Koodin hiomista, triggereitä

---
+++ Snake 3D/Assets/Scripts/BodyTriggers.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BodyTriggers : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+        print("GAME OVER");
+    }
+}
--- Snake 3D/Assets/Scripts/GameManager.cs
+++ Snake 3D/Assets/Scripts/GameManager.cs
@@ -7,16 +7,20 @@ public class GameManager : MonoBehaviour {
     public float tickDuration = 1f;
     float time;
 
-    public GameObject snakeBodiesInstance;
-    public GameObject foodPrefab;
+    public int boardLength;
+    public int boardHeight;
 
-    List<GameObject> bodyList;
-    GameObject[] food;
+    public GameObject snakeBodiesPrefab;
+    public GameObject foodInstance;
+    public List<GameObject> bodyList;
 
-    enum Direction { Up, Down, Right, Left};
+    enum Direction { Up, Down, Right, Left };
     Direction deniedDirection;
 
     Vector3 moveDir;
+    Vector3 snakeEnd;
+
+    bool foodEaten;
 
 
 
@@ -31,18 +35,19 @@ public class GameManager : MonoBehaviour {
         time += Time.deltaTime;
 
         Controls();
-        FoodEaten();
+        FoodEater();
 
         while (time > tickDuration) {
-            snakeBodiesInstance.transform.position += moveDir;
+            BodyMover();
+            BodyGrower();
             time -= tickDuration;
         }
     }
 
-    
-    
-    
-    
+
+
+
+
     //Player input - arrow keys to move, can't move backwards
     void Controls() {
         if (Input.GetKey("up") && deniedDirection != Direction.Up) {
@@ -63,12 +68,38 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    //Food is moved around when touched by the snake, snake grows in size
-    void FoodEaten() {
-        food = GameObject.FindGameObjectsWithTag("Food");
-
-        if (Mathf.Round(Vector3.Distance(snakeBodiesInstance.transform.position, food[0].transform.position)) == 0) {
-            food[0].transform.position = new Vector3(Random.Range(-14, 14), 0.001f, Random.Range(-12, 12));
+    void FoodEater() {
+        if (Mathf.Round(Vector3.Distance(bodyList[0].transform.position, foodInstance.transform.position)) == 0) {                      //Are Food and Snake close together?
+            foodInstance.transform.position = RandomPosition();
+            foodEaten = true;
         }
+    }
+
+    void BodyMover() {
+        //Move the first part of the snake
+        bodyList[0].transform.position += moveDir;
+
+        //Move the rest of the parts
+        snakeEnd = bodyList[bodyList.Count - 1].transform.position;
+
+        bodyList[bodyList.Count - 1].transform.position = bodyList[0].transform.position - moveDir;
+        bodyList.Insert(1, bodyList[bodyList.Count - 1]);
+        bodyList.RemoveAt(bodyList.Count - 1);
+    }
+
+    void BodyGrower() {
+        if (foodEaten == true) {
+            GameObject newPart = Instantiate(snakeBodiesPrefab, snakeEnd, Quaternion.identity) as GameObject;                           //Increase snake length upon eating food
+            bodyList.Add(newPart);
+            foodEaten = false;
+        }
+    }
+
+    Vector3 RandomPosition() {
+        int randomX = Random.Range(-boardLength / 2, boardLength / 2);
+        int randomY = Random.Range(-boardHeight / 2, boardHeight / 2);
+
+        Vector3 pos = new Vector3(randomX, 0, randomY);
+        return pos;
     }
 }
